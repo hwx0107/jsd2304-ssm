59920c8dc30d6c76bcfa1f2fc22fcbf1c79aac13
成恒 <chengheng@tedu.cn>
2023-06-09 16:48:31 +0800

    封装DBUtil

---
+++ _02JDBC/src/main/java/jdbc/DBUtil.java
@@ -0,0 +1,36 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * 数据库连接工具
+ */
+
+public class DBUtil {
+    static {
+        try {
+            // 加载数据库驱动
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                // 注意更改库名为:tedu ,此项配置直接到文档服务器中复制
+                "jdbc:mysql://localhost:3306/tedu?characterEncoding=utf8&useSSL=false&serverTimezone=Asia/Shanghai&rewriteBatchedStatements=true",
+                "root",
+                "root"
+        );
+    }
+}
+
+
+
+
+
+
+
